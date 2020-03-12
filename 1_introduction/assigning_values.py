7d4455167b16c21bcf8510ac4bac33e6cdf89c6e
techworldguru <61414720+techworldguru@users.noreply.github.com>
2020-03-12 07:06:44 +0530

    Add files via upload

---
+++ 1_introduction/assigning_values.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# assign variables
+
+counter = 100
+miles = 1000.0
+name = "Giri"
+
+# to print the variables
+
+print("Variable integer :", counter)
+print("Variable float :", miles )
+print("Variable string :", name)
+
+
+#multiple assignment applicable
+
+var1 = var2 = var3 = 90
+
+#to check the variable names
+print("var1 :",var1)
+print("var2 :",var2)
+print("var3 :",var3)
+
+var1,var2,var3 = 80,34.45,"Giridhar"
+
+print("var1 :",var1)
+print("var2 :",var2)
+print("var3 :",var3)
+
+
+
+
+
+
---
+++ 1_introduction/conditional.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+# conditional statetements in python
+
+var = 100
+if var :
+    print("A true expresssion value")
+    print(var)
+
+# if and else statement
+# the value var is null or zero so
+# the condition is not satisfied and moved to else statement
+
+var = 0
+if var :
+    # two while spces, type the code    
+    print("A true expression value")
+    print(var)
+else:
+    print("Good bye.!!")
+
+# while statement
+
+count = 0
+while (count < 5):
+    print('The count is :', count)
+    count = count + 1
+
+print("Good bye..!!")
+
+
+# arithmetic progressions using range method with for loop
+
+for var in range(5):
+    print(var)
